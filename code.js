0d5f5c695265c23bda6998b7ce483bd648cbb0e8
faten25 <127453968+faten25@users.noreply.github.com>
2023-03-17 21:49:17 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,1 @@
-const demo = document.querySelector(".demo");
 
-demo.addEventListener("click", ({ target }) => {
-    const bloc = target.closest("li");
-    bloc.classList.toggle("overlay");
-}, { passive: true });
